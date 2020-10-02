5f245dd18cbd40f6c04a3b957f763978c505ec7e
Lester016 <lesterdeguzman1999@gmail.com>
2020-10-02 14:29:16 +0800

    Implement auth: redux

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { compose, combineReducers, createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { persistReducer, persistStore } from "redux-persist";
+import AsyncStorage from "@react-native-community/async-storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { createFilter } from "redux-persist-transform-filter";
+
+import HomeScreen from "./app/screens/HomeScreen";
+import { watchAuth } from "./app/store/sagas";
+import authReducer from "./app/store/reducers/auth";
+
+// For redux devtools.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// store only a subset of your state of reducer one
+const saveSubsetFilter = createFilter("auth", ["token", "userID"]);
+
+const persistConfig = {
+  // Root
+  key: "root",
+  // Storage Method (React Native)
+  storage: AsyncStorage,
+  whitelist: ["auth"], // auth reducer will be persisted
+  transforms: [saveSubsetFilter],
+};
+
+// Collections of reducer.
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+// Middleware: Redux Persist Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Instantiate saga.
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+// Middleware: Redux Persist Persistor
+let persistor = persistStore(store);
+
+// Run sagas
+sagaMiddleware.run(watchAuth);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/store/actions/index.js
@@ -0,0 +1,1 @@
+export { authenticate, authLogout } from "./auth";
