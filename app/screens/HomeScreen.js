2e84db2af2a2cbd6d463230389018e88b88e98c6
Lester016 <lesterdeguzman1999@gmail.com>
2020-10-05 13:24:41 +0800

    Upgrade expo

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -3,12 +3,26 @@ import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={[{ fontSize: 30 }, styles.text]}>Group 10</Text>
+      <Text style={styles.text}>Ballarta, Jonas Andrei</Text>
+      <Text style={styles.text}>Caunar, John Joseph</Text>
+      <Text style={styles.text}>De Guzman, Lester</Text>
+      <Text style={styles.text}>Pilapil, Kim Kenneth</Text>
+      <Text style={styles.text}>Vinzon, Julianne Rose</Text>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+  },
+});
