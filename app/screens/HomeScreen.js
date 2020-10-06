b362a9f87553cbbb44684e109f4eff1f8d1653cf
Lester016 <lesterdeguzman1999@gmail.com>
2020-10-06 17:32:16 +0800

    Create Screen component

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+
+import Screen from "../components/Screen";
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
+    <Screen style={styles.container}>
       <Text style={[{ fontSize: 30 }, styles.text]}>Group 10</Text>
       <Text style={styles.text}>Ballarta, Jonas Andrei</Text>
       <Text style={styles.text}>Caunar, John Joseph</Text>
       <Text style={styles.text}>De Guzman, Lester</Text>
       <Text style={styles.text}>Pilapil, Kim Kenneth</Text>
       <Text style={styles.text}>Vinzon, Julianne Rose</Text>
-    </View>
+    </Screen>
   );
 };
 
@@ -18,7 +20,6 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignSelf: "center",
     justifyContent: "center",
   },
